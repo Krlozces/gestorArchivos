dd00e7ca8e153e3dfd794de6492dd296c9f7a64f
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-09-09 23:35:15 -0500

    getTableQuery function modified

--- app/Filament/Resources/ArchivoResource/Pages/ListArchivos.php
+++ app/Filament/Resources/ArchivoResource/Pages/ListArchivos.php
@@ -1,15 +1,53 @@
 <?php
 
 namespace App\Filament\Resources\ArchivoResource\Pages;
+use Illuminate\Support\Facades\Log;
 
-use App\Filament\Resources\ArchivoResource;
 use Filament\Actions;
+use App\Models\Archivo;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\ArchivoResource;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ListArchivos extends ListRecords
 {
     protected static string $resource = ArchivoResource::class;
 
+    protected function getTableQuery(): Builder
+    {
+        $query = Archivo::query();
+        $user = auth()->user();
+    
+        // Logueamos el permiso de gerencia como ejemplo
+        Log::info('Usuario: ' . $user->name . ' - Permiso view_management_archivo: ' . ($user->can('view_management_archivo') ? 'Sí' : 'No'));
+    
+        $areas = [
+            'scale_archivo' => 'Balanzas',
+            'assistant_archivo' => 'Asistente de gerencia',
+            'management_archivo' => 'Gerencia',
+            'treasury_archivo' => 'Tesoreria',
+            'accounting_archivo' => 'Contabilidad',
+            'resources_archivo' => 'Recursos humanos',
+            'maintenance_archivo' => 'Mantenimiento',
+            'sanitation_archivo' => 'Calidad y Saneamiento',
+            'hotel_archivo' => 'Hotel',
+        ];
+    
+        // Aplicamos los filtros basados en permisos
+        $query->where(function (Builder $query) use ($user, $areas) {
+            foreach ($areas as $permission => $areaName) {
+                if ($user->can($permission)) {
+                    $query->orWhereHas('area', function (Builder $query) use ($areaName) {
+                        $query->where('nombre', $areaName);
+                    });
+                }
+            }
+        });
+    
+        return $query;
+    }
+    
     protected function getHeaderActions(): array
     {
         return [
